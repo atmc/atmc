131fbf078ea2c54c738ab5d88a7fcf0600261df5
Tiago Souza <tiago@etc.digital>
2020-12-07 20:13:56 -0300

    feat(mege-styles): add new package to merge styles rules

---
+++ packages/merge-styles/@types/index.d.ts
@@ -0,0 +1,3 @@
+import { CSSRules } from "@atmc/rules";
+export declare const merge: (target: any, ...sources: any) => any;
+export declare const objectsMerge: (objects: CSSRules[]) => any;
---
+++ packages/merge-styles/src/index.ts
@@ -0,0 +1,34 @@
+import { CSSRules } from "@atmc/rules";
+
+const isObject = (item: any) => item && typeof item === "object" && !Array.isArray(item);
+
+export const merge = (target: any, ...sources: any): any => {
+	if (!sources.length) {
+		return target;
+	}
+
+	const source = sources.shift();
+
+	if (isObject(target) && isObject(source)) {
+		for (const key in source) {
+			if (isObject(source[key])) {
+				if (!target[key]) {
+					Object.assign(target, { [key]: {} });
+				}
+				merge(target[key], source[key]);
+			} else {
+				Object.assign(target, { [key]: source[key] });
+			}
+		}
+	}
+
+	return merge(target, ...sources);
+};
+
+export const objectsMerge = (objects: CSSRules[]): CSSRules => {
+	let rules: CSSRules = {};
+	objects.forEach((rule: CSSRules) => {
+		rules = merge(rules, rule);
+	});
+	return rules;
+};
