3da985d7836d543f3e5a0d733c589e877ae4b79d
Tiago Souza <tiago@etc.digital>
2020-12-08 06:45:33 -0300

    fix(react): :ambulance: fixex re-hydratation of styles

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,7 +1,14 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { getStyleString } from "@atmc/css";
 
 const getStyle: FC = () => {
+	const [_, setIsClient] = useState<boolean>(false);
+
+	useEffect(() => {
+		// Fix re-hydratation
+		setIsClient(true);
+	}, []);
+
 	return <style id="_cssatom" dangerouslySetInnerHTML={{ __html: getStyleString() }} />;
 };
 
