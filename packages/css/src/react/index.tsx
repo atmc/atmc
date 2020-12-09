365a1d3cdd2f341445d8d15024053d58add22081
Tiago Souza <tiago@etc.digital>
2020-12-09 17:00:42 -0300

    feat(react): convert ReactStyle to ClientStyle and StyleTag

--- packages/css/src/react/index.tsx
+++ packages/css/src/react/index.tsx
@@ -1,30 +1,28 @@
-import React, { FC } from "react";
+import React, { FC, Fragment } from "react";
 import { getStyleString } from "../";
 
 const styleID = "_atmc";
 
-const getElement = (): HTMLStyleElement => {
+export const ClientStyle: FC = () => {
+	if (typeof window === "undefined") {
+		return <Fragment />;
+	}
 	// Hydrate existing style element if available
 	let el = document.getElementById(styleID) as HTMLStyleElement;
 	if (el) {
-		return el;
+		el.innerHTML = getStyleString();
+	} else {
+		// Create a new one otherwise
+		el = document.createElement("style");
+		el.id = styleID;
+
+		// Avoid Edge bug where empty style elements don't create sheets
+		el.appendChild(document.createTextNode(getStyleString()));
+		document.head.appendChild(el);
 	}
-
-	// Create a new one otherwise
-	el = document.createElement("style");
-	el.id = styleID;
-
-	// Avoid Edge bug where empty style elements don't create sheets
-	el.appendChild(document.createTextNode(""));
-	document.head.appendChild(el);
-	return el;
+	return <Fragment />;
 };
 
-export const ReactStyle: FC = () => {
-	if (window) {
-		const element = getElement();
-		element.innerHTML = getStyleString();
-		return <></>;
-	}
+export const StyleTag: FC = () => {
 	return <style id={styleID} dangerouslySetInnerHTML={{ __html: getStyleString() }} />;
 };
