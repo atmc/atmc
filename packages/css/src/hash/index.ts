ebea7f1845765c97f6f9706bcc19a2821808f4a1
Tiago Souza <tiago@etc.digital>
2020-12-11 09:07:17 -0300

    refactor(css): replace duplicate to consts

--- packages/css/src/hash/index.ts
+++ packages/css/src/hash/index.ts
@@ -5,28 +5,32 @@
  */
 
 const hasher = (str: string): string => {
+	const z = 0x5bd1e995;
+	const y = 0xe995;
+	const x = 16;
+	const w = 0xffff;
+	const v = 24;
+
 	const c = (s: string, i: number) => s.charCodeAt(i) & 0xff;
-
-	const g = (k: any) => (k & 0xffff) * 0x5bd1e995 + (((k >>> 16) * 0xe995) << 16);
-
-	const j = (h: any) => (h & 0xffff) * 0x5bd1e995 + (((h >>> 16) * 0xe995) << 16);
+	const g = (k: any) => (k & w) * z + (((k >>> x) * y) << x);
+	const j = (h: any) => (h & w) * z + (((h >>> x) * y) << x);
 
 	let h = 0;
 	let k;
 	let i = 0;
 	let len = str.length;
 	for (; len >= 4; ++i, len -= 4) {
-		k = c(str, i) | (c(str, ++i) << 8) | (c(str, ++i) << 16) | (c(str, ++i) << 24);
+		k = c(str, i) | (c(str, ++i) << 8) | (c(str, ++i) << x) | (c(str, ++i) << v);
 
 		k = g(k);
-		k ^= k >>> 24;
+		k ^= k >>> v;
 
 		h = g(k) ^ j(h);
 	}
 
 	switch (len) {
 		case 3:
-			h ^= c(str, i + 2) << 16;
+			h ^= c(str, i + 2) << x;
 		case 2:
 			h ^= c(str, i + 1) << 8;
 		case 1:
